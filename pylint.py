d91391bd21db9fc4938de5895e8a31db29b5a7e9
Quentin de Metz <quentin.de-metz@pricematch.travel>
2014-09-01 18:37:49 +0200

    new pylint

--- pylint.py
+++ pylint.py
@@ -4,4 +4,4 @@ from subprocess import check_call
 from sys import argv
 
 if __name__ == '__main__':
-    check_call('pylint {}'.format(argv[1]), shell=True)
+    check_call('pylint {}'.format(' '.join(argv[1:])), shell=True)
